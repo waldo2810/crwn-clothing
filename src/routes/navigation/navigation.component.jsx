0a324244d07136c1ed61c733afefee7e935503ec
Waldo Sobrino <waldo2810@gmail.com>
2023-02-04 12:00:24 -0500

    add navbar user displayName

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,22 @@
-import { Fragment, useContext } from 'react';
-import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Fragment, useContext } from "react";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { CartContext } from '../../contexts/cart.context';
-import { selectCurrentUser } from '../../store/user/user.selector';
+import { CartContext } from "../../contexts/cart.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import {
   NavigationContainer,
   NavLinks,
   NavLink,
   LogoContainer,
-} from './navigation.styles';
+} from "./navigation.styles";
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
@@ -29,6 +29,11 @@ const Navigation = () => {
           <CrwnLogo className='logo' />
         </LogoContainer>
         <NavLinks>
+          {currentUser && (
+            <span style={{ padding: "10px 15px" }}>
+              {currentUser.displayName.toUpperCase()}
+            </span>
+          )}
           <NavLink to='/shop'>SHOP</NavLink>
 
           {currentUser ? (
