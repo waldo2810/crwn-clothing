d3afe866d07d3a30f1be1b7701e352f927c54687
Waldo Sobrino <waldo2810@gmail.com>
2023-02-11 00:19:05 -0500

    fix stripe integration

--- netlify/functions/create-payments-intent.js
+++ netlify/functions/create-payments-intent.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (e) => {
+exports.handler = async (event) => {
   try {
-    const { amount } = JSON.parse(e.body);
+    const { amount } = JSON.parse(event.body);
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
@@ -11,10 +11,16 @@ exports.handler = async (e) => {
       payment_method_types: ["card"],
     });
 
-    return { statusCode: 200, body: JSON.stringify({ paymentIntent }) };
-  } catch (err) {
-    console.log(err);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log({ error });
 
-    return { status: 400, body: JSON.stringify({ err }) };
+    return {
+      status: 400,
+      body: JSON.stringify({ error }),
+    };
   }
 };
