81ce0c4f5edb7b299c2b19193f440dd0154c4ccd
Waldo Sobrino <waldo2810@gmail.com>
2023-02-11 19:13:42 -0500

    add close cart once checkout click

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,8 +1,13 @@
 import { useCallback } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import { selectCartItems } from "../../store/cart/cart.selector";
+import {
+  selectCartItems,
+  selectIsCartOpen,
+} from "../../store/cart/cart.selector";
+
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -15,10 +20,13 @@ import {
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const goToCheckoutHandler = useCallback(() => {
+    dispatch(setIsCartOpen(false));
     navigate("/checkout");
   }, []);
 
